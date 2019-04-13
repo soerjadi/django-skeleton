5c7cc669d6506c4373560affcf0e594a39e80bcc
surya <soerja.610@gmail.com>
2019-04-13 23:37:23 +0700

    add api application

---
+++ api/modules/info.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Suryadi Sulaksono
+"""
+
+from rest_framework import generics
+from rest_framework.response import Response
+
+
+class InfoView(generics.RetrieveAPIView):
+
+    def get(self, request, *args, **kwargs):
+        return Response({'description': 'success'})
---
+++ api/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Suryadi Sulaksono
+"""
+
+from django.conf.urls import url
+
+urlpatterns = [
+    url(r'info$')
+]
